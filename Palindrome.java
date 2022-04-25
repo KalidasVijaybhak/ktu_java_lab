c49e9262ba1e3e9af4852a1593d1ce80e016c7f0
Kalidas VijayBhak <calidazvb@gmail.com>
2022-04-25 09:48:50 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Palindrome{
+    public static void main(String args[]){
+         System.out.println(checkPalindrome("malayalam"));
+    }
+      static boolean checkPalindrome(String x){
+    char l;
+    String s = "";
+    int lenght = x.length()-1;
+    int i = 0;
+    while(k>=0){
+        s+= x.charAt(k);
+         
+        length--;
+    }
+    if(x.equals(s)){
+        return true;
+    }
+    return false;
+}
+ 
+}
+
